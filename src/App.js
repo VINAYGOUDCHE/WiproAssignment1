5d3083663f3eee9c4adc08c7d6019e40a6a71ee1
Vinay <vinaygoudch87@gmail.com>
2025-08-28 08:54:52 +0530

    Changes Made

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import FlightTicket from './Day2ReactFiles/Lab15';
 import MovieForm from './Day2ReactFiles/Lab16';
 
 import Lab18 from './Day2ReactFiles/Lab18';
-import Lab17 from './Day2ReactFiles/Lab17';
 
 
 
@@ -118,11 +117,6 @@ function App() {
       <div>
         <MovieForm/>
       </div>
-      {/*-------Lab17-------*/}
-      <div>
-        <Lab17/>
-      </div>
-      {/*-------Lab18-------*/}
       <div>
         <Lab18/>
       </div>
